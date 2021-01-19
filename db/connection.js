7eb5566a268de4795d755a0b9a9c85028b8d69cd
McGyver105 <steve.titchener@hotmail.com>
2021-01-19 15:57:06 +0000

    adds the listen file and start script

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,18 @@
 const knex = require('knex');
-const dbconfig = require('../knexfile');
+const ENV = process.env.NODE_ENV;
+
+const dbconfig = 
+    ENV === 'production' ?
+        {
+            client: 'pg',
+            connection: {
+                connectionString: process.env.DATABASE_URL,
+                ssl: {
+                    rejectUnauthorized: false,
+                }
+            }
+        }
+        : require('../knexfile')
 
 const connection = knex(dbconfig);
 
